63ba411047fda5615929b6e5a8b8e3db2906bba7
Jon Schipp <jonschipp@gmail.com>
2014-11-15 15:14:26 -0600

    provision.sh: enable mod_cgi

--- nagios/provision.sh
+++ nagios/provision.sh
@@ -49,7 +49,7 @@ then
                 make install-config &&
                 install -c -m 644 $HOME/nagios.conf /etc/apache2/sites-available/nagios.conf &&
                 install -c -m 644 $HOME/ports.conf /etc/apache2/ports.conf &&
-		make install-exfoliation && a2ensite nagios.conf && a2enmod ssl &&
+		make install-exfoliation && a2ensite nagios.conf && a2enmod cgi ssl &&
                 htpasswd -bc /usr/local/nagios/etc/htpasswd.users nagiosadmin badpassword && service apache2 restart
 
         fi
