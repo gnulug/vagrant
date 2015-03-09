c2fbe91fd9d13d1696864611f30386e93c1844be
Jon Schipp <jonschipp@gmail.com>
2015-03-08 21:18:55 -0500

    new.sh: print to do

--- new.sh
+++ new.sh
@@ -7,3 +7,11 @@ cp -r base $new
 [ -d ${new}/.vagrant ] && rm -r ${new}/.vagrant
 [ -f ${new}/Vagrantfile ] && [ "$uname" = "Darwin" ] && sed -i.bu "/name/s/ubuntu/$new/" ${new}/Vagrantfile && rm -f ${new}/Vagrantfile.bu
 [ -f ${new}/Vagrantfile ] && [ "$uname" = "Linux" ]  && sed -i "/name/s/ubuntu/$new/" ${new}/Vagrantfile
+
+cat <<EOF
+Do the following things:
+  * Edit README.md with information and instructions
+  * Edit the Vagrantfile: change vb.name
+  * Automate the provisioning of the server by editing provision.sh
+  * Commit back to repo for review
+EOF
